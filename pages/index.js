f2dda00ffb9725d884e7a80b46c19f5616bf43e7
cdmccauley <cdmccauley@gmail.com>
2023-02-02 19:55:25 -0700

    material ui in place

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,42 @@ import { useEffect, useState } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
+// import styles from "../styles/Home.module.css";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import theme from "../styles/theme";
 
 import clientPromise from "../lib/mongodb";
 
+import crypto from "crypto";
+import { Container } from "@mui/material";
+
 export default function Home(props) {
   const [config, setConfig] = useState({ title: "", description: "" });
+  const [clientId, setClientId] = useState("");
 
   useEffect(() => {
     if (props.config) setConfig(JSON.parse(props.config));
+    if (props.clientId) setClientId(props.clientId);
   }, []);
 
   useEffect(() => {
-    if (config.fun) console.info(config.fun)
-  }, [config])
+    if (config.fun) console.info(config.fun);
+  }, [config]);
+
+  useEffect(() => {
+    if (process.env.PUBLIC_VERCEL_ENV != "production" && clientId)
+      console.info(clientId);
+  }, [clientId]);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>{config.title}</title>
         <meta name="description" content={config.description} />
@@ -42,10 +61,18 @@ export default function Home(props) {
         />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <main className={styles.main}>
-        <Image src="/android-chrome-512x512.png" width={256} height={256} alt="" />
-      </main>
-    </>
+      <CssBaseline enableColorScheme />
+      <Container
+        sx={{ display: "flex", justifyContent: "center", padding: "24px" }}
+      >
+        <Image
+          src="/android-chrome-512x512.png"
+          width={256}
+          height={256}
+          alt=""
+        />
+      </Container>
+    </ThemeProvider>
   );
 }
 
@@ -55,8 +82,21 @@ export async function getServerSideProps(context) {
     const db = await client.db("frontend");
     const collection = await db.collection("serversideprops");
     const config = await collection.findOne({ name: "config" });
+
+    const clientId = crypto.randomUUID();
+
+    const clients = await db.collection("clients");
+    await clients.insertOne({
+      _id: clientId,
+      created: new Date().valueOf(),
+    });
+
     return {
-      props: { isConnected: true, config: JSON.stringify(config) },
+      props: {
+        isConnected: true,
+        config: JSON.stringify(config),
+        clientId: clientId,
+      },
     };
   } catch (e) {
     console.error(e);
---
+++ styles/theme.js
@@ -0,0 +1,30 @@
+import { grey } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#556cd6",
+//     },
+//     secondary: {
+//       main: "#19857b",
+//     },
+//     error: {
+//       main: red.A400,
+//     },
+//   },
+// });
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#47236b",
+    },
+  },
+});
+
+export default theme;
+
+// https://mui.com/material-ui/customization/dark-mode/
